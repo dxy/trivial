626d1ce0a5233c5623d52c0124db228e2d75534c
Daisuke Yabuki <dxy@google.com>
2009-06-11 17:04:22 +0900

    correction and disambiguation in wording

--- epoch.py
+++ epoch.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2009 Daisuke Yabuki. All Rights Reserved.
 
-"""Print local time and day for a given number of seconds since UNIX epoch."""
+"""Print local time and day for a given number of seconds since UNIX epoch.
+
+   i.e. date -d @1234567890
+        date -d '1970-01-01 1234567890 sec GMT'
+"""
 
 __author__ = 'dxy@acm.org (Daisuke Yabuki)'
 
@@ -12,7 +16,7 @@ import time
 
 
 def main():
-  usage = 'usage: $prog seconds_since_epoch'
+  usage = 'usage: $prog seconds_since_epoch (i.e. posix timestamp)'
   parser = OptionParser(usage=usage)
   (unused_options, args) = parser.parse_args()
 
@@ -20,16 +24,16 @@ def main():
     parser.error('one and only one argument expected')
 
   try:
-    epoch = float(args[0])
+    posix_timestamp = float(args[0])
   except ValueError:
     print 'unable to parse the argument'
     sys.exit(1)
 
   (year, month, day,
    hour, minute, second,
-   unused_wday, unused_yday, unused_isdst) = time.localtime(epoch)
+   unused_wday, unused_yday, unused_isdst) = time.localtime(posix_timestamp)
 
-  print '%d/%d/%d %d:%d:%d' % (year, month, day, hour, minute, second)
+  print '%d/%02d/%02d %02d:%02d:%02d' % (year, month, day, hour, minute, second)
 
 if __name__ == '__main__':
   main()
